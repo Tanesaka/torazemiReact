9b63c06512edfea6ea769201d5d79e13e5b42db3
Tanesaka <seedtanesa@outlook.jp>
2021-02-20 11:45:46 +0900

    Torazemi5

--- src/Article.jsx
+++ src/Article.jsx
@@ -4,6 +4,8 @@ const Article = (props) => {
   return (
     <div>
       <h2>{props.title}</h2>
+      <label htmlFor="check">公開状態：</label>
+      <input type="checkbox" checked={props.isPublished} id="check" onClick={() => props.toggle()}/>
     </div>
   )
 };
--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,11 +1,23 @@
 
+import { findAllByDisplayValue } from "@testing-library/react";
 import React from "react";
 import Article from "./Article";
 
 class Blog extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isPublished: false
+    }
   }
+
+  // 公開状態を反転させる関数
+  togglePublished = () => {
+    this.setState({
+      isPublished: !this.state.isPublished
+    })
+  };
+
   render() {
     const authorName = "Trahack"
     return (
@@ -13,12 +25,8 @@ class Blog extends React.Component {
       <>
         <Article
           title={"Reactの使い方"}
-        />
-        <Article
-          title={"JSXの使い方"}
-        />
-        <Article
-          title={"環境構築してみよう"}
+          isPublished={this.state.isPublished}
+          toggle={() => this.togglePublished()}
         />
       </>
     )
