8639461981f3164b1e7b146c81a60cb3cb82d2c8
Tanesaka <seedtanesa@outlook.jp>
2021-02-23 12:10:03 +0900

    Torazemi8.7

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,15 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import LikeButton from "./LikeButton";
 
 const Article = (props) => {
+  const [isPublished, togglePublished] = useState(false);
   return (
     <div>
       <h2>{props.title}</h2>
       <label htmlFor="check">公開状態：</label>
-      <input type="checkbox" checked={props.isPublished} id="check" onClick={() => props.toggle()}/>
-      <LikeButton
-        count={props.count}
-      />
+      <input type="checkbox" checked={isPublished} id="check" onClick={() => togglePublished(!isPublished)}/>
+      <LikeButton />
     </div>
   )
 };
--- src/Blog.jsx
+++ src/Blog.jsx
@@ -5,53 +5,56 @@ import Article from "./Article";
 import * as FooBar from "./components/FooBar";
 import Hoge from "./components/hoge";
 
-class Blog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isPublished: false,
-      count: 0
-    }
-  }
+// class Blog extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       isPublished: false,
+//       count: 0
+//     }
+//   }
 
-  componentDidMount() {
-    // ボタンがクリックされたらいいねをカウントアップする
-    document.getElementById("counter").addEventListener("click", this.countUp)
-  }
+const Blog = () => {
 
-  componentDidUpdate() {
-    if (this.state.count >= 10) {
-      this.setState({count: 0})
-    }
-  }
 
-  // ページ遷移すると、このカウントアップメゾッドがcounterIDから外れる
-  // （目視では確認できないが、サーバーの負荷を減らせる）
-  componentWillUnmount() {
-    document.getElementById("counter").removeEventListener("click", this.countUp)
-  }
+  // componentDidMount() {
+  //   // ボタンがクリックされたらいいねをカウントアップする
+  //   document.getElementById("counter").addEventListener("click", this.countUp)
+  // }
+
+  // componentDidUpdate() {
+  //   if (this.state.count >= 10) {
+  //     this.setState({count: 0})
+  //   }
+  // }
+
+  // // ページ遷移すると、このカウントアップメゾッドがcounterIDから外れる
+  // // （目視では確認できないが、サーバーの負荷を減らせる）
+  // componentWillUnmount() {
+  //   document.getElementById("counter").removeEventListener("click", this.countUp)
+  // }
 
   // 公開状態を反転させる関数
-  togglePublished = () => {
-    this.setState({
-      isPublished: !this.state.isPublished
-    })
-  };
+  // togglePublished = () => {
+  //   this.setState({
+  //     isPublished: !this.state.isPublished
+  //   })
+  // };
 
-  countUp = () => {
-    this.setState({count: this.state.count + 1})
-  }
+  // countUp = () => {
+  //   this.setState({count: this.state.count + 1})
+  // }
 
-  render() {
-    const authorName = "Trahack"
+  // render() {
+  //   const authorName = "Trahack"
     return (
       // React.Fragmentを入れるとタグで囲まなくていい<>でもいい
       <>
         <Article
           title={"Reactの使い方"}
-          isPublished={this.state.isPublished}
-          toggle={() => this.togglePublished()}
-          count={this.state.count}
+          // isPublished={this.state.isPublished}
+          // toggle={() => this.togglePublished()}
+          // count={this.state.count}
         />
         <FooBar.Foo />
         <FooBar.Bar />
@@ -59,6 +62,6 @@ class Blog extends React.Component {
       </>
     )
   }
-}
+
 
 export default Blog;
